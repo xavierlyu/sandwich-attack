95763bab47c242c82d1e41e43a363bbaa4d0cfe7
Xavier Lu <xll2@cornell.edu>
2021-05-01 16:12:06 -0400

    added calling swapETHForExactTokens

--- handleTransaction.js
+++ handleTransaction.js
@@ -19,7 +19,7 @@ const web3 = new Web3(new AWSHttpProvider(HTTP_ENDPOINT));
 
 let uniswapRouter;
 
-export default function handleTransaction(
+export default async function handleTransaction(
   UNISWAP_ROUTER,
   UNISWAP_FACTORY,
   user_wallet,
@@ -51,12 +51,56 @@ export default function handleTransaction(
   }
 
   console.log(transaction);
-  console.log(JSON.stringify(decodedData));
+  // console.log(JSON.stringify(decodedData));
+
+  let to = decodedData.inputs[1];
+
   let amountOut =
     parseInt(JSON.stringify(decodedData.inputs[0]).slice(1, -1), 16) / WEI;
   let targetToken = decodedData.inputs[1][1];
-  console.log(amountOut);
+
+  // TODO double check if `targetToken` is not a stablecoin
+
+  // console.log(amountOut);
 
   let gasPrice = parseInt(transaction["gasPrice"]);
   let newGasPrice = gasPrice * 2;
+
+  to = to.map((address) => "0x" + address);
+
+  const tokenAmount = await UNISWAP_ROUTER.methods
+    .getAmountsOut(web3.utils.toWei("1", "Ether"), to)
+    .call();
+
+  console.log(tokenAmount[1].toString());
+
+  // calculating deadline
+  await web3.eth.getBlock("latest", (error, block) => {
+    deadline = block.timestamp + 300; // transaction expires in 300 seconds (5 minutes)
+  });
+
+  deadline = web3.utils.toHex(deadline);
+
+  let swap = UNISWAP_ROUTER.methods.swapETHForExactTokens(
+    tokenAmount[1].toString(),
+    to,
+    user_wallet.address,
+    deadline
+  );
+
+  let encodedABI = swap.encodeABI();
+
+  let tx = {
+    from: user_wallet.address,
+    to: UNISWAP_ROUTER_ADDRESS,
+    gas: (300000).toString(),
+    gasPrice: newGasPrice,
+    data: encodedABI,
+    value: 0.0036 * WEI, // idk lol this is like $10 USD
+  };
+
+  let signedTx = user_wallet.signTransaction(tx);
+
+  console.log("ye");
+  console.log(signedTx);
 }
